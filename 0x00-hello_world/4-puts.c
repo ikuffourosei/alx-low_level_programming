b168857cbe29c03919d9adb6c9ec6e784f4710e3
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-08-10 18:54:09 -0700

    printing using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ * main function is the entry point
+ *
+ * puts returns positive integer for succes, negative for failure
+ */
+int main() 
+{
+	const char *message = "\"Programming is like building a multilingual puzzle";
+
+	puts(message);
+
+	return(0);
+}
