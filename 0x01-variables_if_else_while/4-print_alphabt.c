672d51642da0335654b66c305b1b266f9f81fdd3
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-08-14 16:57:16 -0700

    printing all alphabets excep q and u

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main -  entry point
+ * Description: A  program that prints every alphabet
+ * except q and e
+ * While loop - iterates over the statements till condtion is false
+ * If - if statement is true, execute this code
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+			c++;
+		}
+	}
+	putchar (c);
+	return (0);
+}
