d3e3d68f092fe7a580ec04c9ab1ad6f79c1a0de9
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-08-14 17:30:28 -0700

    printing decimals less than 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: A C program that prints decimal digits (0-9)
+ * While loop -  iterate till condition is false
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int d;
+
+	d = 0;
+	while (d <= 9)
+	{
+		printf("%d\n", d);
+		d++;
+	}
+	return (0);
+}
