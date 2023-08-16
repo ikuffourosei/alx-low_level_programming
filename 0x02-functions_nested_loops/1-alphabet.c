1390418062698e777eb97905e3aafc45f6940b0c
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-08-15 19:22:03 -0700

    printing alphabets with a prototype function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,7 @@
 #include "main.h"
 
 /**
- * void - function with no return value
  * print_alphabet - prints lowercase characters followed by newline
- * Description: print_alphabet() displays the lowercase
- * alphabet in sequential order,starting from 'a' and ending with 'z',
- * followed by a newline character
  * Return: no value
  */
 void print_alphabet(void)
