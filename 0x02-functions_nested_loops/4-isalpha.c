c65764d4b2f70b401013dd97886902cfd666dd8f
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-08-16 08:03:18 -0700

    checks if an input is a character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ *_isalpha - checks if an input is a alphabet
+ * @c: parameter to be checked
+ *
+ *Return: 1 (true) and 0 (otherwise)
+ */
+int _isalpha(int c)
+{
+	return (isalpha(c));
+}
