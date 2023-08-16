9eeaffd9b9aa8ec11dcd8cf4b5bc274f23663f9c
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-08-15 17:29:03 -0700

    using function prototype "_putchar" to print characters

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: using _putchar function prototype to
+ * print strings
+ * Return: 0 on success
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
