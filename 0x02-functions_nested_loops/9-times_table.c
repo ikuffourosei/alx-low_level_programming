bd63187d72e88c7f61f32c1f449b3712ec0e55bf
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-08-16 18:16:34 -0700

    print multiples of 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *times_table - prints multiple of 9
+ *
+ * Return: none
+ */
+void times_table(void)
+{
+	int s, e;
+
+	for (s = 0; s <= 9; s++)
+	{
+		for (e = 0; e <= 9; e++)
+		{
+			int m, f, l;
+
+			m = s * e;
+			f = m / 10;
+			l = m % 10;
+			if (m < 10)
+			{
+				_putchar('0' + m);
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('0' + f);
+				_putchar('0' + l);
+			}
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
