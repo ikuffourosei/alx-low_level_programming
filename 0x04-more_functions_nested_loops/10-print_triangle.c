3b8b9451a821ac9124771f22a97e471008435a8b
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-08-18 16:53:07 -0700

    printing traingles according to size

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_traingle - prints traingle
+ * @size: parameter to be checked
+ */
+void print_triangle(int size)
+{
+	int c, d;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (c = 1; c <= size; c++)
+	{
+		for (d = 1; d <= (size - c); d++)
+		{
+			_putchar(' ');
+		}
+		for (d = 1; d <= c; d++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
