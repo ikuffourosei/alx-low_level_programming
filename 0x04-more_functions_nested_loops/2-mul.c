f8948771c566373bd00a991385b1435ca1f54133
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-08-18 07:31:21 -0700

    multiple of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul -  multiply two integers
+ * @a: first parameter to be checked
+ * @b: second parameter to be checked
+ *
+ *Return: a * b
+ */
+int mul(int a, int b)
+{
+	int m;
+
+	m = a * b;
+	return (m);
+}
