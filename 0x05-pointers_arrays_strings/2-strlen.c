e0b0c2ddac67aa6f2e5835429f3db48971f9a888
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-08-21 05:44:42 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns length of string
+ * @s: parameter to be checked
+ * Return: 1 (success)
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
