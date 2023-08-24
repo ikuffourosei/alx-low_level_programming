c5006ad474d3f76b8b173785748f51cf43ac075e
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-08-24 13:53:08 -0700

    copying string from src to dest using _strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy -  copies string from src to dest
+ * @dest: destination string
+ * @src: source string
+ * @n: maximum bytes to be used from src
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
