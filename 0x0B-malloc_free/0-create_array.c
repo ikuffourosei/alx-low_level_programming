8314b051504e7691fa28529563aa941f8acdc8c8
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-09-04 18:51:37 -0700

    allocating memory for an array and initiliazing with a special character

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * create_array - function that creates an array of chars,
+ * and initializes it with a specific char
+ * @size: size of array
+ * @c: character
+ * Return: character or NULL
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ch;
+	unsigned int j;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	ch = malloc((size + 1) * sizeof(char));
+
+	for (j = 0; j < size; j++)
+	{
+		ch[j] = c;
+	}
+	ch[size] = '\0';
+
+	return (ch);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+This directory contains C program files on memory allocation
+Using malloc and free
