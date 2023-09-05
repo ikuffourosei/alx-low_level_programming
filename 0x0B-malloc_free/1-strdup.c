663628cf7c3dd9a4775a4a25d2eeb6bdfc765496
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-09-05 05:15:45 -0700

    allocate memory for a string array which points to a duplicate of a string gien as a parameter in the function prototype

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a duplicate of a string
+ * @str: string
+ * Return: NULL or character value
+ */
+char *_strdup(char *str)
+{
+	unsigned int i, len;
+	char *new;
+
+	len = strlen(str);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	new = malloc(len * sizeof(char));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		new[i] = str[i];
+	}
+	new[len] = '\0';
+
+	return (new);
+}
