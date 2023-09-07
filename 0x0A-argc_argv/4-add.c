92b3c0df669009e71cd4747f27ca17001f2b0fa1
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-09-06 20:03:26 -0700

    adding positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**
+ * main - entry point
+ * @argc: count the number of arguments
+ * @argv: string array
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum, num, j;
+	char *arr;
+	bool validInput;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return 0;
+	}
+	sum = 0;
+	validInput = true;
+       	for (i = 1; i < argc; i++)
+	{
+		arr = argv[i];
+		for (j = 0; arr[j] != '\0'; j++)
+		{
+			if (arr[j] < '0' || arr[j] > '9')
+			{
+				validInput = false;
+				break;
+			}
+		}
+		if (validInput)
+		{
+			num = atoi(arr);
+			if (num >= 0)
+			{
+				sum += num;
+			}
+			else
+			{
+				validInput = false;
+			}
+		}
+		if (!validInput)
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
