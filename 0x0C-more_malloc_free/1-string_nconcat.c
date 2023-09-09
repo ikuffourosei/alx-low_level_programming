cf006d981cdb9ec6865d494bbe6dd06d0c2f51d5
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-09-08 18:00:04 -0700

    allocating memory for string concatenation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,8 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
+ 
 /**
  * string_nconcat - concatenate two strings
  * @s1: destination string
@@ -13,10 +11,9 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	unsigned int len1, i, len2;
+	unsigned int len1, i, j, len2;
 
-	len1 = strlen(s1);
-	len2 =  strlen(s2);
+	j = 0;
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -25,27 +22,33 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
+	len1 = 0;
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+	len2 = 0;
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
 	if (n >= len2)
 	{
-		len1 = len1 + len2;
+		n = len2;
 	}
-	else if (n < len2)
-	{
-		len1 = len1 + n;
-	}
-	ptr = malloc((len1 + 1) * sizeof(char));
+	ptr = malloc((len1 + n + 1) * sizeof(char));
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < n; i++)
+	for (i = 0; i < len1; i++)
 	{
 		ptr[i] = s1[i];
 	}
-	for (i = 0; i < n && s2[i] != '\0'; i++)
+	for (i = len1; i < len1 + n; i++)
 	{
-		ptr[len1 + i] = s2[i];
+		ptr[i] = s2[j++];
 	}
-	ptr[len1 + i] = '\0';
+	ptr[i] = '\0';
 	return (ptr);
 }
