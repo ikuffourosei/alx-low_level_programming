81967096f6ca41de3da2c88067d80f6d8f33143c
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-09-11 15:27:46 -0700

    structure for dog information

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - contains information about dog and owner
+ * @name: first member
+ * @age: second member
+ * @owner: thrid member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
