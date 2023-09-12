055acf308f1a2c877106c82885969ba50539552d
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-09-11 17:49:37 -0700

    defining a new alias for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @age: second member
  * @owner: thrid member
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
