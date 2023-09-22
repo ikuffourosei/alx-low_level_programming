98f3caef5fcbf4e0103615f4356c257de4086f92
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-09-22 07:41:56 -0700

    print singly liked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,12 +3,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
-typedef struct list_s {
+/**
+ * struct list_s - a struct
+ * @str: string
+ * @len: length
+ * @next: pointer to struct list_s
+ */
+typedef struct list_s
+{
 	char *str;
 	int len;
 	struct list_s *next;
-}list_t;
+} list_t;
 
 size_t print_list(const list_t *h);
 
