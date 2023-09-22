4a0dc1a5eaee607fe0de35d4d06b8cf2fd29ff8c
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-09-22 07:58:22 -0700

    function that returns the number of elements

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stddef.h>
 /**
  * struct list_s - a struct
  * @str: string
@@ -17,5 +18,6 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif
