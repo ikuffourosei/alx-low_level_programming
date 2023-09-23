65e7b8f7cab7f669d4140ec3dea2245f49d338bd
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-09-22 18:52:25 -0700

    opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -5,22 +5,22 @@
  * main - entry point
  * @argc: argument count
  * @argv: argument vector
- * Return integer value
+ * Return: integer value
  */
 int main(int argc, char *argv[])
 {
 	unsigned char *opcode;
 	void (*main_f)();
 	int n, i;
-	
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	
+
 	n = atoi(argv[1]);
-	
+
 	if (n < 0)
 	{
 		printf("Error\n");
