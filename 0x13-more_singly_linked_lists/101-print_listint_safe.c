14927343ea2c6ac627caf2a77f44475add07e08b
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-09-26 18:06:27 -0700

    prints a list safely

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,32 +1,60 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - a function that prints a listint_t linked list.
- * @head: Pointer to the head of the list.
- * Return: The number of nodes in the list.
+ * re_alloc - reallocates memory for an array of pointers
+ * to the nodes in a linked list
+ * @list: previous list
+ * @size: size of the new list
+ * @new: new node to be added to the list
+ * Return: pointer to newly formed list
+ */
+const listint_t **re_alloc(const listint_t **list, size_t size, const listint_t *new)
+{
+	const listint_t **n_list;
+	unsigned int i;
+
+	n_list = malloc(size * sizeof(listint_t *));
+	if (n_list == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+	{
+		n_list[i] = list[i];
+	}
+	n_list[i] = new;
+	free(list);
+	return (n_list);
+}
+
+/**
+ * print_listint_safe -a function that prints a listint_t linked list.
+ * @head: pointer to the first node of the list
+ * Return: the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	unsigned int index, ncount;
-	const listint_t *current, *visited[1024];
+	unsigned int i, ncount;
+	const listint_t **list = NULL;
 
 	ncount = 0;
-	current = head;
-	visited[1024] = NULL;
-	while (current != NULL)
+	while (head != NULL)
 	{
-		index = ((size_t)current) % 1024;
-		if (visited[index] == current)
+		for (i = 0; i < ncount; i++)
 		{
-			printf("-> [%p] %d\n", (void *)current, current->n);
-			printf("-> [%p] %d\n", (void *)current, current->n);
-			exit(98);
+			if (head == list[i])
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(list);
+				return (ncount);
+			}
 		}
-		visited[index] = current;
-		printf("[%p] %d\n", (void *)current, current->n);
-
-		current = current->next;
 		ncount++;
+		list = re_alloc(list, ncount, head);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 	}
+	free(list);
 	return (ncount);
 }
